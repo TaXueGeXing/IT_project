3536f9dd22b6520ce0fd7d137b68e09f19edf651
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-05 02:03:55 +0800

    Update models.py

--- car_wiki/models.py
+++ car_wiki/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    carID = models.CharField(max_length=8)
+    car_model = models.TextField()
+    define = models.TextField()
+    brand = models.TextField()
