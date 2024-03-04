6275efd4f26253a55689f37026f5cf5350299d63
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-05 02:03:25 +0800

    Update urls.py

--- IT_Project/urls.py
+++ IT_Project/urls.py
@@ -19,4 +19,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('ranking/', views.home, name='home'),
+    path('carwiki/', views.carwiki, name='carwiki'),
+    path('search/car/', views.search_car, name='search_car'),
+    path('car_detail/<str:car_id>/', views.car_detail, name='car_detail'),
+    path('search/product/', views.search_product, name='search_product'),
+    path('about-us/', views.about_us, name='about_us'),
 ]
