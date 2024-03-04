67a2902ab9c6a12429dbb26b6476444c389904de
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-05 02:05:23 +0800

    Update models.py

--- home_page/models.py
+++ home_page/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    productID = models.CharField(max_length=8)
+    time = models.DateTimeField()
+    price = models.IntegerField()
+    description = models.TextField()
+    title = models.TextField()
+    sellerID = models.CharField(max_length=8)
+    car = models.ForeignKey("Car", on_delete=models.CASCADE)
+    # 关联？
+
+
+class Article(models.Model):
+    articleID = models.CharField(max_length=8)
+    title = models.TextField()
+
+
+class Comment(models.Model):
+    commentID = models.CharField(max_length=8)
