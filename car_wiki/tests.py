20d4e548a7d094ed9a2aae05a21fdd47cf89bb04
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-05 20:01:13 +0800

    Update tests.py

--- car_wiki/tests.py
+++ car_wiki/tests.py
@@ -27,5 +27,45 @@ class CarModelTestCase(TestCase):
 # 使用以下命令运行测试
 # python manage.py test your_app_name
 
+from django.test import TestCase
 
+from car_wiki.models import Car, CarDetail, Image
+
+# 照片测试代码
+class CarDetailImageTestCase(TestCase):
+
+    def setUp(self):
+        # 创建测试数据
+        self.car1 = Car.objects.create(CarModel='Test Model', Brand='Test Brand')
+        self.car2 = Car.objects.create(CarModel='Model', Brand='Brand')
+        self.car_detail1 = CarDetail.objects.create(Car=self.car1, Define='Test Definition')
+        self.car_detail2 = CarDetail.objects.create(Car=self.car2, Define='Test')
+        self.image1 = Image.objects.create(CarDetail=self.car_detail1, picture='path/to/test_image1.jpg')
+        self.image2 = Image.objects.create(CarDetail=self.car_detail1, picture='path/to/test_image2.jpg')
+        self.image3 = Image.objects.create(CarDetail=self.car_detail1, picture='path/to/test_image3.jpg')
+        self.image4 = Image.objects.create(CarDetail=self.car_detail2, picture='test_image4.jpg')
+        self.image5 = Image.objects.create(CarDetail=self.car_detail2, picture='path/to/test_image5.jpg')
+
+    def test_car_detail_images(self):
+        # 断言关系是否正确
+        self.assertEqual(self.car1.car_detail, self.car_detail1)
+        self.assertEqual(self.car_detail2.Car, self.car2)
+
+        # 获取关联的照片列表
+        images1 = self.car_detail1.images.all()
+        images2 = self.car_detail2.images.all()
+
+        # 断言照片数量是否正确
+        self.assertEqual(images1.count(), 3)
+        self.assertEqual(images2.count(), 2)
+        expected_image_name = 'test_image4.jpg'
+        self.assertEqual(images2.first().picture.name, expected_image_name)
+
+        # 断言每个照片是否关联到正确的 CarDetail
+        for image in images1:
+            self.assertEqual(image.CarDetail, self.car_detail1)
+            print(f"Image {image.id} CarDetail: {image.CarDetail}, Expected CarDetail: {self.car_detail1}")
+        for image in images2:
+            self.assertEqual(image.CarDetail, self.car_detail2)
+            print(f"Image {image.id} CarDetail: {image.CarDetail}, Expected CarDetail: {self.car_detail2}")
 # Create your tests here.
