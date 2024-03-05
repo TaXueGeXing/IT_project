b76b6b5d8b7cb1d5cf55cea021a2f8a757cb4716
Zonglin Wu <2882018W\@student.gla.ac.uk>
2024-03-06 00:04:57 +0800

    modify details

--- car_wiki/urls.py
+++ car_wiki/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('car_wiki/', views.car_wiki, name='car_wiki'),
                   path('search/car/', views.search_car, name='search_car'),
-                  path('car_detail/<int:car_id>/', views.car_detail, name='car_detail'),
+                  path('car_detail/<int:car_id>/', views.car_detail_view, name='car_detail'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
