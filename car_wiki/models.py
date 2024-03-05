c0cd201746e81ffea0659b060ee3de3bd02adb4a
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-05 17:41:59 +0800

    Update models.py

--- car_wiki/models.py
+++ car_wiki/models.py
@@ -5,6 +5,6 @@ class Car(models.Model):
     Type = models.CharField(max_length=30)
     Define = models.CharField(max_length=30)
     Brand = models.CharField(max_length=30)
-
+    Image = models.ImageField(upload_to='images', blank=True, null=True)
 # Create your models here.
 
