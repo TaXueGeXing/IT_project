fef54b0ade082f53056f752c580dcdf9f01bab39
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-05 17:16:42 +0800

    Update models.py

--- home_page/models.py
+++ home_page/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
-# Create your models here.
-class Product(models.Model):
-    productID = models.CharField(max_length=8)
-    time = models.DateTimeField()
-    price = models.IntegerField()
-    description = models.TextField()
-    title = models.TextField()
-    sellerID = models.CharField(max_length=8)
-    # car = models.ForeignKey("Car", on_delete=models.CASCADE)
-    # 关联？ 我暂时调整了代码，因为这行代码会导致报错
 
+class Car(models.Model):
+    carID = models.CharField(max_length=8)
+    carModel = models.TextField()
+    define = models.TextField()
+    brand = models.TextField()
+
+
+class Product(models.Model):
+    ProductID = models.AutoField(primary_key=True)
+    time = models.DateTimeField()
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    Title = models.CharField(max_length=100)
+    SellerID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    car = models.ForeignKey("Car", on_delete=models.PROTECT)
+    我暂时调整了代码，因为这行代码会导致报错
 
 class Article(models.Model):
-    articleID = models.CharField(max_length=8)
-    title = models.TextField()
+    ArticleID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=100)
+    Content = models.TextField()
+    Tag = models.CharField(max_length=30)
 
 
-class Comment(models.Model):
-    commentID = models.CharField(max_length=8)
+class Reply(models.Model):
+    ReplyID = models.AutoField(primary_key=True)
+    ArticleID = models.ForeignKey(Article, on_delete=models.CASCADE)
+    Time = models.DateTimeField()
