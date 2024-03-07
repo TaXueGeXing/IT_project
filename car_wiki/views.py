4cad1f957000d31e22432414805b088048b26777
Zonglin Wu <2882018W\@student.gla.ac.uk>
2024-03-08 05:35:03 +0800

    modify details

--- car_wiki/views.py
+++ car_wiki/views.py
@@ -1,55 +1,38 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Car, CarDetail,Image
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Car, CarDetail, Image
+from .serializers import CarSerializer
 
 
+@api_view(['GET'])
 def car_wiki(request):
-    return render(request, 'carwiki.html')
+    cars = Car.objects.all()
+    serializer = CarSerializer(cars, many=True)
+    return Response(serializer.data)
 
 
-def carwiki_view(request):
-    # 获取默认产品（最新发布的五种车）
-    default_cars = Car.objects.filter(is_default=True).order_by('-create_time')[:5]
-    # 获取搜索结果
-    result_car = request.session.get('result_car', None)
-
-    # 如果没有搜索结果，使用默认产品
-    if result_car is None:
-        result_car = default_cars
-
-    context = {'result_car': result_car}
-
-    return render(request, 'carwiki.html', context)
-
-
+@api_view(['GET'])
 def search_car(request):
-    # 处理搜索请求
     if request.method == 'GET':
         brand = request.GET.get('Brand')
         car_model = request.GET.get('CarModel')
 
-        # 处理搜索结果
         result_car = Car.objects.filter(
-            # 根据实际情况调整筛选条件
             Brand__icontains=brand,
             CarModel__icontains=car_model,
         )
 
         if result_car.exists():
-            car_id = result_car.first().CarID
-            return redirect('car_detail', car_id=car_id)
-        # 如果没有搜索结果停留
-        default_cars = Car.objects.filter(is_default=True).order_by('-create_time')[:5]
-        context = {'default_cars': default_cars}
-        return render(request, 'carwiki.html', context)
+            serializer = CarSerializer(result_car.first())
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
+@api_view(['GET'])
 def car_detail_view(request, car_id):
-    # 获取特定汽车的信息
-    car = get_object_or_404(Car, CarID=car_id)
-    # 获取关联的CarDetail对象
-    car_detail = get_object_or_404(CarDetail, Car=car)
-    # 获取关联的Image对象列表
-    images = Image.objects.filter(CarDetail=car_detail)
-
-    context = {'car': car, 'car_detail': car_detail, 'images': images, }
-    return render(request, 'car_detail.html', context)
+    car = get_object_or_404(Car, car_id=car_id)
+    serializer = CarSerializer(car)
+    return Response(serializer.data)
