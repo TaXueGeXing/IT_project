231f3b9dc44730794ca85de928556eb4f593ccc6
ZlW20000 <159026315+ZlW20000@users.noreply.github.com>
2024-03-09 02:01:03 +0800

    Update models.py

--- community/models.py
+++ community/models.py
@@ -2,31 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100) 
-
-    def __str__(self):
-        return self.name
 
 class Article(models.Model):
     article_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
     title = models.CharField(max_length=100)
     content = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name='articles')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    views_count = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30)
+    created_time = models.DateTimeField(auto_now_add=True)
+    click = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.title
 
 class Reply(models.Model):
     reply_id = models.AutoField(primary_key=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='replies')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_replies')
+    content = models.TextField()
     time = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return f"Reply to {self.article.title} by {self.article.user.username} at {self.time}"
+
