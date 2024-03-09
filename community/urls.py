a19d1af9e56ea59df0b3c68158577f8ed72cd86f
GalaxyIceMaple <577423639@qq.com>
2024-03-09 13:50:33 +0000

    IX

--- community/urls.py
+++ community/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('community', views.article_list, name='article_list'),
-    path('article_list', views.article_list, name='article_list'),
-    path('article/<int:article_id>/', views.article_detail, name='article_detail'),
-    path('create/', views.create_article, name='create_article'),
-    path('article/<int:article_id>/reply/', views.create_reply, name='create_reply')
+    path('community/', views.community_view, name='community_view'),
+    path('articles/create/', views.create_article, name='create_article'),
+    path('articles/<int:pk>/replies/create/', views.create_reply, name='create_reply'),
+    path('articles/', views.article_list, name='article_list'),
+    path('articles/search/', views.search_articles, name='search_articles'),
+    path('articles/<int:pk>/', views.article_detail, name='article_detail'),
 ]
