686a5c9a98470bf2c28cd7abe97365214b78a0f8
Zonglin Wu <2882018W\@student.gla.ac.uk>
2024-03-13 01:57:18 +0800

    Add comment

--- car_wiki/views.py
+++ car_wiki/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
-from .models import Car, CarDetail
+from .models import Car
 from .serializers import CarSerializer
 
 
 @api_view(['GET'])
-def car_wiki(request):
-    cars = Car.objects.all()
+def car_wiki(request):  # Car wiki page
+    cars = Car.objects.all()[:3]  # Show three cars on Car wiki page
     serializer = CarSerializer(cars, many=True)
     return Response(serializer.data)
 
@@ -16,16 +15,16 @@ def car_wiki(request):
 @api_view(['GET'])
 def search_car(request):
     if request.method == 'GET':
-        brand = request.GET.get('car_brand')
-        car_model = request.GET.get('car_model')
+        brand = request.GET.get('car_brand')  # Get car brand
+        car_model = request.GET.get('car_model')  # Get car model
 
-        result_car = Car.objects.get(car_brand=brand, car_model=car_model)
+        result_car = Car.objects.get(car_brand=brand, car_model=car_model)  # Get the matching car object
         serializer = CarSerializer(result_car)
         return Response(serializer.data)
 
 
 @api_view(['GET'])
 def car_detail_view(request, car_id):
-    car = get_object_or_404(Car, car_id=car_id)
+    car = get_object_or_404(Car, car_id=car_id)   # Get the specified car object with car id, If not, return 404 NOT FOUND
     serializer = CarSerializer(car)
     return Response(serializer.data)
