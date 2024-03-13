8b4034675f7deb0efd73bf1be8a86d3ac0115357
GalaxyIceMaple <577423639@qq.com>
2024-03-13 14:01:21 +0000

    XX

--- account/serializers.py
+++ account/serializers.py
@@ -5,13 +5,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
